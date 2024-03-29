fd4b4c3f098c57fed25f27dee2d77ff1f59e727a
Theo RAHARISON <theoraha@gmail.com>
2024-03-29 09:44:11 +0100

    lien page d'acceuil

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignInOrLogInComponent } from './sign-in-or-log-in/sign-in-or-log-in.component';
+import { AccueilComponent } from './accueil/accueil.component';
 
 import { SignInComponent } from './sign-in/sign-in.component';
 
-const routes: Routes = [{path:'login',component:LoginComponent},{path:'signin',component:SignInComponent},{path:"**",component:SignInOrLogInComponent}];
+const routes: Routes = [{path:'login',component:LoginComponent},{path:'signin',component:SignInComponent},{path:"**",component:AccueilComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
