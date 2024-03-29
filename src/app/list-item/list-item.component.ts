3303a26c7446328c21bddcec3039bddd99a1b42b
maxime123987 <maxime.d.02@free.fr>
2024-03-29 09:02:53 +0100

    select qui fonctionne

--- src/app/list-item/list-item.component.ts
+++ src/app/list-item/list-item.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 
 
+
+
 @Component({
   selector: 'app-list-item',
   templateUrl: './list-item.component.html',
   styleUrl: './list-item.component.css'
 })
-export class ListItemComponent {
 
+export class ListItemComponent {
+  
 }
