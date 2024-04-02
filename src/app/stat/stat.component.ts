8aa240b1e3950293693dac933b24de3c2d651616
Nightskare <rom59143@outlook.fr>
2024-04-02 12:08:34 +0200

    up stat

--- src/app/stat/stat.component.ts
+++ src/app/stat/stat.component.ts
@@ -1,8 +1,6 @@
 import { Component,ViewChild  } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
-import { ApiService } from '../api.service';
-import {MatTable, MatTableDataSource} from '@angular/material/table';
-import { Classes } from '../classes';
+import { StatServiceService } from '../stat-service.service';
 @Component({
   selector: 'app-stat',
   templateUrl: './stat.component.html',
@@ -10,22 +8,32 @@ import { Classes } from '../classes';
 })
 export class StatComponent {
   level = new FormControl('');
-  displayedColumns: string[] = ['data.name', 'data.image', 'data.description', 'data.stat'];
-  @ViewChild(MatTable) table !: MatTable<any>;
-  Classes = new MatTableDataSource<Classes>();
-  constructor(public apiService : ApiService) {}
-  ngOnInit(){
-    this.getAllClasses();
+  vigorUpdate: number;
+  espritUpdate: number;
+  enduranceUpdate: number;
+  forceUpdate: number;
+  dexteriteUpdate: number;
+  intelligenceUpdate: number;
+  foiUpdate: number;
+  esoterismeUpdate: number;
+  constructor(private service:StatServiceService){
+    this.vigorUpdate=0;
+    this.espritUpdate=0;
+    this.enduranceUpdate=0;
+    this.forceUpdate=0;
+    this.dexteriteUpdate=0;
+    this.intelligenceUpdate=0;
+    this.foiUpdate=0;
+    this.esoterismeUpdate=0;
   }
-
-  public getAllClasses() {
-    this.apiService.getIds("classes").subscribe(a => {
-      for(let i : number = 0; i < a.count; i++){
-        this.apiService.getClasses(a.data[i].id).subscribe(b => {
-          this.Classes.data.push(b);
-          this.table.renderRows();
-        });
-      }
-    });
+  ngOnInit(): void{
+    this.service.vigorUpdate$.subscribe(vigorUpdate=>this.vigorUpdate=vigorUpdate)
+    this.service.espritUpdate$.subscribe(espritUpdate=>this.espritUpdate=espritUpdate)
+    this.service.enduranceUpdate$.subscribe(enduranceUpdate=>this.enduranceUpdate=enduranceUpdate)
+    this.service.forceUpdate$.subscribe(forceUpdate=>this.forceUpdate=forceUpdate)
+    this.service.dexteriteUpdate$.subscribe(dexteriteUpdate=>this.dexteriteUpdate=dexteriteUpdate)
+    this.service.intelligenceUpdate$.subscribe(intelligenceUpdate=>this.intelligenceUpdate=intelligenceUpdate)
+    this.service.foiUpdate$.subscribe(foiUpdate=>this.foiUpdate=foiUpdate)
+    this.service.esoterismeUpdate$.subscribe(esoterismeUpdate=>this.esoterismeUpdate=esoterismeUpdate)
   }
 }
