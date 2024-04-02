2dcd719a749988b64aa0054627798d71366813e2
The10axe <the10axe@gmail.com>
2024-04-02 11:28:46 +0200

    Fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,12 +8,9 @@ import { BuildComponent } from './build/build.component';
 import { ListItemComponent } from './list-item/list-item.component';
 import { CharacterComponent } from './character/character.component';
 
-<<<<<<< HEAD
 
-const routes: Routes = [{path:'login',component:LoginComponent},{path:'ListeItem',component:ListItemComponent},{path:"api", component:TestComponent},{path:'signin',component:SignInComponent}, {path:"**",component:AccueilComponent}];
-=======
-const routes: Routes = [{path:'login',component:LoginComponent}, {path:"api", component:TestComponent},{path:'signin',component:SignInComponent},{path:'build',component:BuildComponent},{path:'character',component:BuildComponent},{path:"**",component:AccueilComponent}];
->>>>>>> dfd2d26fca784a79d90f19da5e8531d2468a54ed
+const routes: Routes = [{path:'login',component:LoginComponent},{path:'ListeItem',component:ListItemComponent},{path:"api", component:TestComponent},{path:'signin',component:SignInComponent},{path:'build',component:BuildComponent},{path:'character',component:BuildComponent}, {path:"**",component:AccueilComponent}];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
