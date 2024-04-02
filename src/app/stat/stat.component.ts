5b3740770c5f76992e8c07e76be7519cd5b87079
Nightskare <rom59143@outlook.fr>
2024-04-02 10:40:40 +0200

    jo

--- src/app/stat/stat.component.ts
+++ src/app/stat/stat.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild  } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { ApiService } from '../api.service';
+import {MatTable, MatTableDataSource} from '@angular/material/table';
+import { Classes } from '../classes';
 @Component({
   selector: 'app-stat',
   templateUrl: './stat.component.html',
@@ -7,4 +10,22 @@ import { FormControl, ReactiveFormsModule } from '@angular/forms';
 })
 export class StatComponent {
   level = new FormControl('');
+  displayedColumns: string[] = ['data.name', 'data.image', 'data.description', 'data.stat'];
+  @ViewChild(MatTable) table !: MatTable<any>;
+  dataSource = new MatTableDataSource<Classes>();
+  constructor(public apiService : ApiService) {}
+  ngOnInit(){
+    this.getAllClasses();
+  }
+
+  public getAllClasses() {
+    this.apiService.getIds("classes").subscribe(a => {
+      for(let i : number = 0; i < a.count; i++){
+        this.apiService.getClasses(a.data[i].id).subscribe(b => {
+          this.dataSource.data.push(b);
+          this.table.renderRows();
+        });
+      }
+    });
+  }
 }
